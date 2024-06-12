e4eac8358448cda2e6d011b9dec791d3f12c344a
wiehoang <ndhhoang1210@gmail.com>
2024-06-12 22:51:38 +0700

    First commit

---
+++ src/main/java/com/wiehoang/firstMavenProject/FirstMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wiehoang.firstMavenProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMavenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiehoang/firstMavenProject/FirstMavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiehoang.firstMavenProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
